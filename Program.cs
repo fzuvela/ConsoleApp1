3211c0d4494e3e58468219d8c8f93b181942c79e
sara190420 <135627303+sara190420@users.noreply.github.com>
2023-06-06 16:41:36 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Novo!");
+            Console.WriteLine("NovoSara!");
         }
     }
 }
